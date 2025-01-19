533c10db9b8ae85fe490504d744ed63a759d8892
mehran.saghebifard <m.saghebi.dev@gmail.com>
2025-01-19 18:28:37 +0330

    wef

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_DYNAMIC_VALUE: process.env.NEXT_PUBLIC_DYNAMIC_VALUE, // This will read from the runtime environment
+  },
 };
-
+console.log(
+  "NEXT_PUBLIC_DYNAMIC_VALUE:",
+  process.env.NEXT_PUBLIC_DYNAMIC_VALUE
+);
 export default nextConfig;
